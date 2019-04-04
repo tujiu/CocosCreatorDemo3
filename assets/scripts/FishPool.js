c5923189a2155b59d0b87b1d9ca7748ba965dbaf
xiaoqinglin123 <528091679@qq.com>
2019-04-04 12:59:35 +0800

    finish lesson 3

---
+++ assets/scripts/FishPool.js
@@ -0,0 +1,66 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        mPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        mFishPool: {
+            default: null,
+            type: cc.Node
+        },
+        mHook: {
+            default: null,
+            type: cc.Node
+        },
+        mDepth: {
+            default: 0,
+            type: cc.Float,
+            visible: false
+        },
+        mSceneData: {
+            default: null,
+            type: cc.JsonAsset
+        },
+        mIndex: {
+            default: 0,
+            visible: false,
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    update (dt) {
+        this.mDepth = Math.floor(Math.abs(this.mHook.y) / 100);
+        let data = this.mSceneData.json[this.mIndex];
+        if (data != undefined && this.mDepth >= data.depth) {
+            var fish = cc.instantiate(this.mPrefab);
+            cc.loader.loadRes(data.res, cc.SpriteFrame, function(err, spriteFrame) {
+                if (!err) {
+                    fish.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                }
+            })
+            fish.x = Math.random() * 640 - 320;
+            fish.y = this.mHook.y - 480 - 100;
+            this.mFishPool.addChild(fish);
+            this.mIndex++;
+        }
+    },
+});
